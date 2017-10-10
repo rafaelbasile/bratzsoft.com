6b8c643cc2df7bd8bb08f78e01965e0e204bba17
Eduardo Bratz <bratzsoft@bratzsoft.com>
2017-10-10 20:10:17 -0300

    Logging Settings

--- bratzsoft/settings/base.py
+++ bratzsoft/settings/base.py
@@ -218,3 +218,56 @@ if DEBUG == False:
     CSRF_COOKIE_SECURE = True
 else:
     pass
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'filters': {
+       # 'special': {
+        #    '()': 'project.logging.SpecialFilter',
+         #   'foo': 'bar',
+        #},
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR', 
+            'class': 'django.utils.log.AdminEmailHandler',
+            #'filters': ['']
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+            'include_html': True,
+        },
+        'bratzsoft.custom': {
+            'handlers': ['console', 'mail_admins'],
+            'level': 'INFO',
+            #'filters': ['special']
+        }
+    }
+}
