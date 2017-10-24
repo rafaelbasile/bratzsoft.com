c9f819c7de917795408e493a362e490aad23fb01
Eduardo Bratz <ebratz@gmail.com>
2017-10-24 00:39:57 -0200

    Stocks Update

--- bratzsoft/investments/models.py
+++ bratzsoft/investments/models.py
@@ -1,6 +1,85 @@
 """Investments Model."""
 from django.db import models
 
+class CompanySector(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class CompanySubSector(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class CompanySegment(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class ListingSegment(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Stock(models.Model):
+
+    STOCK_TYPE_CHOICES = (
+    ('ON', 'Ordinary'),
+    ('PN', 'Preffered'),
+        )
+
+    symbol = models.CharField(max_length=8)
+    stock_type = models.CharField(max_length=2, choices=STOCK_TYPE_CHOICES)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.symbol
+
+
+class Company(models.Model):
+
+
+    CATEGORY_CHOICES = (
+    ('AL', 'Alerta'),
+    ('NC', 'Não Classificada'),
+    ('EQ', 'Equilibrada'),
+        )
+
+
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=4)
+    stocks = models.ManyToManyField(Stock)
+    cnpj = models.PositiveIntegerField()
+    activity = models.CharField(max_length=100)
+    sector = models.ForeignKey(CompanySector, on_delete=models.CASCADE)
+    subsector = models.ForeignKey(CompanySubSector, on_delete=models.CASCADE)
+    market_segment = models.ForeignKey(CompanySegment, on_delete=models.CASCADE)
+    listing_segment = models.ForeignKey(ListingSegment, on_delete=models.CASCADE)
+    market_value = models.CharField(max_length=100)
+    ev = models.CharField(max_length=100)
+    bookkeeper = models.CharField(max_length=100)
+    majority_shareholder = models.CharField(max_length=100)
+    site = models.URLField()
+    headquarters= models.CharField(max_length=150)
+    bovespa_page = models.URLField()
+    shareholders = models.PositiveIntegerField()
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
+    ibov = models.BooleanField(default=False)
+    next_balance = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Companies"
+
+
+
+
 
 class Loan(models.Model):
     loan_name = models.CharField(max_length=100)
@@ -55,7 +134,7 @@ class Broker(models.Model):
         return self.name
 
 
-class Stock(models.Model):
+class Transaction(models.Model):
     OPERATION_TYPE = (
         ('BUY', 'Buy'),
         ('SEL', 'Sell'),
@@ -63,18 +142,14 @@ class Stock(models.Model):
         ('SPL', 'Split'),
         ('AGR', 'Aggregation'),)
 
-    symbol = models.CharField(max_length=6)
-    name = models.CharField(max_length=100, null=True, blank=True)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     operation_type = models.CharField(null=True,blank=True,max_length=3, choices=OPERATION_TYPE)
     operation_date = models.DateField()
-    quantity = models.PositiveIntegerField()
+    shares = models.PositiveIntegerField()
     price = models.DecimalField(decimal_places=9,max_digits=15)
     commission = models.DecimalField(decimal_places=2,max_digits=6)
     broker = models.ForeignKey(Broker)
     notes = models.TextField(null=True,blank=True)
 
     def __str__(self):
-        return self.symbol
-
-
-
+        return '%s' % (str(self.stock.id))
